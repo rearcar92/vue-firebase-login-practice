9eb3ae6f41838541672c684a001b985d3c8681fc
rearcar92 <rearcar92@gmail.com>
2019-03-15 17:07:32 +0900

    Sign page add

---
+++ src/components/SignUp.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <form>
+       <div>
+            <label for="email">Email :</label>
+            <input type="email" id="email" v-model="email">
+       </div>
+       <div>
+            <label for="password">Password :</label>
+            <input type="password" id="password" v-model="password">
+       </div>
+            <button @click.prevent="signUp">sign up</button>
+   </form>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+  name: 'SignUp',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signUp () {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        .then((user) => {
+          console.log(user)
+          this.$router.replace('login')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/SignView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <sign-up></sign-up>
+    </div>
+</template>
+
+<script>
+import SignUp from '../components/SignUp.vue'
+export default {
+  name: 'SingView',
+  components: {
+    SignUp
+  }
+}
+</script>
+
+<style>
+
+</style>
